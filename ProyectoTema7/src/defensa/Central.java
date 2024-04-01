55ba5c6408d995d00066f516eda0f7b324ecb52d
susana004 <134385230+susana004@users.noreply.github.com>
2024-04-01 20:58:51 +0200

    Update Central.java

--- ProyectoTema7/src/defensa/Central.java
+++ ProyectoTema7/src/defensa/Central.java
@@ -15,4 +15,29 @@ public class Central extends Defensa {
     public void setEntradas(int entradas) {
        this.entradas = entradas;
        }
+
+    @Override
+	public void mostrarDatos() {
+		System.out.println(this.toString());
+	}
+
+    @Override
+	public String toString() {
+		return "Central [nombre= " +this.getNombre() +
+				", dorsal= " + this.getDorsal() +
+				", equipo= " + this.getEquipo() +
+				", disputasRealizadas= " + this.getDisputasRealizadas() +
+				", entradas= " + this.entradas + "]";
+	}
+    
+    @Override
+	public boolean equals (Object obj) {
+		Central otro = (Central) obj;
+		boolean result = false;
+		if(super.equals(otro) && this.entradas == otro.entradas) {
+			result = true;
+		}
+		return result;
+	}
+
 }
